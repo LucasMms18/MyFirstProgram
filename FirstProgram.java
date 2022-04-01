52e9b65ec36e6b69622d76cfe2fc47076c0294eb
unknown <lukas.mathias007@gmail.com>
2022-04-01 11:03:21 -0300

    MyFirstProgram

---
+++ FirstProgram.java
@@ -0,0 +1,15 @@
+public class FirstProgram {
+    public static void main(String[] args) {
+        var firstname = "Lucas";
+        var lastname = "Mathias";
+        var username = "Arkademon";
+        var email = "lukas.mathias007@gmail.com";
+        var password = "123456789";
+        System.out.println(firstname + " " + lastname);
+        System.out.println(username);
+        System.out.println(email);
+        System.out.println(password);
+
+
+    }
+}
